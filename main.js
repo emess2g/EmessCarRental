326ed3ab82032452b197611599aeb4ffd7b39e05
emess2g <essuonemma84@gmail.com>
2023-10-11 13:54:57 +0000

    initial code

---
+++ main.js
@@ -0,0 +1,19 @@
+//  show/hide faq answer
+
+const faq = document.querySelectorAll('.faq');
+
+faq.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+    })
+
+        //change icon
+        const icon = faq.querySelector('.faq-icon i');
+        if(icon.className === 'fa-solid fa-minus'){
+            icon.className = "fa-solid fa-plus";
+        } else {
+            icon.className = "fa-solid fa-plus";
+        }
+})
+
+
